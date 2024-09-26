7e4b038672251f8f3c18aa8dcddc38d8796c1b92
Christina Sørensen <christina@cafkafk.com>
2024-09-26 05:28:35 +0200

    style: switch to nixfmt rfc style, format tree
    Signed-off-by: Christina Sørensen <christina@cafkafk.com>

---
+++ treefmt.nix
@@ -0,0 +1,27 @@
+{
+  projectRootFile = "Cargo.toml";
+  programs = {
+    nixfmt.enable = true; # nix
+    statix.enable = true; # nix static analysis
+    deadnix.enable = true; # find dead nix code
+    rustfmt.enable = true; # rust
+    shellcheck.enable = true; # bash/shell
+    taplo.enable = true; # toml
+    yamlfmt.enable = true; # yaml
+  };
+  settings = {
+    formatter = {
+      shellcheck = {
+        includes = [
+          "*.sh"
+          "./completions/bash/eza"
+        ];
+        excludes = [ ".envrc" ];
+      };
+
+      rustfmt.excludes = [ "src/options/flags.rs" ];
+      taplo.excludes = [ "tests/ptests/*.toml" ];
+      yamlfmt.excludes = [ "./powertest.yaml" ];
+    };
+  };
+}
